7a6d69bd9d4d665ffcefaad28762c57f014955a8
lotiko <l.despres@netc.fr>
2022-01-29 17:07:06 +0100

    table avec create element

--- public/javascripts/cricket.js
+++ public/javascripts/cricket.js
@@ -13,18 +13,21 @@ function event_init() {
     e.preventDefault();
     if (inputPlayer.value === "") return;
     let td = document.createElement("td");
-    tr_name.innerHTML = tr_name.innerHTML + create_td(inputPlayer.value, "name_p");
+    tr_name.append(create_td(inputPlayer.value, "name_p"));
     inputPlayer.value = "";
     let old_nb = Number(nb_player.textContent);
     nb_player.textContent = old_nb + 1;
   };
   start_btn.onclick = (e) => {
-    e.preventDefault();
-    let players = Number(nb_player.textContent);
-    let names = get_names();
-    if (players === 0 || names.length === 0) return;
-    create_cricket(players, names);
+      e.preventDefault();
+      let players = Number(nb_player.textContent);
+      let names = get_names();
+      console.log("in click start", players, names);
 
+      if (players === 0 || names.length === 0) return;
+      create_cricket(players, names);
+    //   body_cricket.innerHTML = "";
+    //   body_cricket.append(table);
   };
 }
 function get_names() {
@@ -34,46 +37,41 @@ function get_names() {
   return ret;
 }
 function create_cricket(nb_player, names) {
-  console.log("in create crik", nb_player, names);
-  let tbody = cricket_values.map((val) => create_tr_cricket(val, names)).join("");
-  let thead = create_tr_head_cricket(names);
-  let innerTable = `
-    <table>
-        <thead>
-            ${thead}
-        </thead>
-        <tbody>
-            ${tbody}
-        </tbody>
-    </table>`;
-    body_cricket.innerHTML = innerTable;
-    console.log(typeof innerTable, body_cricket.innerHTML);
+    console.log("in create crik", nb_player, names);
+    let table = document.createElement("table");
+    body_cricket.append(table);
+    let thead = table.createTHead();
+    let tbody = table.createTBody();
+    thead.append(create_tr_head_cricket(nb_player, names));
+    cricket_values.forEach(el => tbody.append(create_tr_cricket(el,nb_player)));
+    console.log(body_cricket.innerHTML);
+   body_cricket.innerHTML = table.outerHTML
 }
-function create_tr_head_cricket(names) {
-  let str = "<tr>";
-  str += create_th("x", "grey");
-  names.forEach((el) => {
-    str += create_th(el, "grey");
-  });
-  str += "</tr>";
-  return str;
+function create_tr_head_cricket(nb_player, names, thead) {
+    let tr = document.createElement('tr');
+    tr.append(create_td("", 'grey',true));
+    names.map(el => {
+        tr.append(create_td(el, 'grey', true));
+    });
+    return tr;
 }
-function create_tr_cricket(first_td_content, names) {
-  let str = "<tr>";
-  str += create_td(first_td_content);
-//   while (nb_player !== 0) {
-//     str += create_td("", "cell_scores");
-//     nb_player -= 1;
-//   }
-    names.forEach(el => str += create_td("", "cell_scores"));
-  str += "</tr>";
-  return str;
+function create_tr_cricket(first_td_content,nb_player) {
+    let tr = document.createElement('tr');
+    tr.append(create_td(first_td_content));
+    while (nb_player !== 0) {
+        tr.appendChild(create_td("", "cell_scores"));
+        nb_player -= 1;
+    }
+    return tr;
+
+
 }
-function create_td(content, className = "") {
-  return `<td class="${className}">${content}</td>`;
-}
-function create_th(content, className = "") {
-  return `<th class="${className}">${content}</th>`;
+function create_td(content, className = "", head = false) {
+  let str_td = head ? "th" : "td";
+  let td = document.createElement(str_td);
+  td.className = className;
+  td.textContent = content;
+  return td;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
